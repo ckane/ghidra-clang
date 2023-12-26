b896da03ab87d6cb6a1190467d221a9733961aa9
Coleman Kane <kaneca@mail.uc.edu>
2023-12-25 19:27:47 -0500

    Updates now support C17

--- process_code.sh
+++ process_code.sh
@@ -2,7 +2,7 @@
 code_dir="$1"
 min_size="$2"
 ls -1 "${code_dir}" | (while read r; do
-  clang -cc1 -fsyntax-only -load ./StmtParser.so -std=c89 -plugin parse-stmts -E -include nocode.h "${code_dir}/$r" `llvm-config --cflags` | (while read p; do
+  clang -cc1 -fsyntax-only -load ./StmtParser.so -std=c17 -plugin parse-stmts -E -include nocode.h "${code_dir}/$r" `llvm-config --cflags` | (while read p; do
   # Old C++ version that would be too strict around syntax
   #clang++ -cc1 -x c++ -fsyntax-only -load ./StmtParser.so -std=gnu++11 -plugin parse-stmts -E -include nocode.h "${code_dir}/$r" `llvm-config --cflags` | (while read p; do
         func=`echo $p | cut -d: -f 1`
